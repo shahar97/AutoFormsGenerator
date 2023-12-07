fd38b391fd205b5ebe8dbb41e80c5701b7e4c46a
shahar <sshtokhamer@gmail.con>
2023-12-07 17:28:07 +0200

    # support nan values

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ from pathlib import Path
 import pandas as pd
 from docxtpl import DocxTemplate
 
+WORD_TEMPLATE = "volunteer_referral_form.docx"
 columns_mapping = {
-    'תאריך': 'Date',
-    'שם וחתימת נותן ההפניה': 'name and signature',
     'לתקופה של': 'period',
     'החל ביום': 'starting',
-    'למען (לציין זהות הגוף או האדם שלמענו נעשית הפעולה ומקום הפעולה):': 'association',
     'התנדב/ה לעבוד בתפקיד': 'Role',
-    'מלא תאריך סטטוס רישום': 'date and status',
     'דואר אלקטרוני': 'Email',
     'טלפון נייד': 'number',
-    'יישוב': 'City',
+    'ישוב': 'City',
     'רחוב, דירה ומספר בית': 'address',
-    'שם': 'full name',
+    'שם פרטי ומשפחה': 'full name',
     'מספר זהות': 'ID'
 
 }
+
+columns_to_drop = ['Event Id', 'Id', 'חותמת', 'שם', 'שם וחתימת נותן ההפניה',
+                   'למען (לציין זהות הגוף או האדם שלמענו נעשית הפעולה ומקום הפעולה):', 'תאריך',
+                   'מלא תאריך סטטוס רישום']
 
 
 # Get the big list of all full names.
@@ -27,10 +28,10 @@ def filter_empty_string(list_of_strings: list):
     return list(filter(lambda x: len(x) > 1, list_of_strings))
 
 
-# Replace space holders in word docx with values from excel .
+# Replace space holders in word docx with values from Excel .
 def replacement(data: pd.DataFrame, output_path: Path):
     # generate doc
-    doc = DocxTemplate("volunteer_referral_form.docx")
+    doc = DocxTemplate(WORD_TEMPLATE)
     for index, volunteer in data.iterrows():
         doc.render(volunteer.to_dict())
         doc_name = output_path / f'{volunteer.ID}.docx'
@@ -45,7 +46,7 @@ def split_full_name(data: pd.DataFrame) -> pd.DataFrame:
     # first_and_last now is big list of small lists - list of all full names of ech volunteer.
     first_and_last = first_and_last.apply(filter_empty_string)
     first_name = first_and_last.apply(lambda x: x[0])
-    last_name = first_and_last.apply(lambda x: x[1])
+    last_name = first_and_last.apply(lambda x: x[-1])
     data['First'] = first_name
     data['Last'] = last_name
 
@@ -55,13 +56,13 @@ def split_full_name(data: pd.DataFrame) -> pd.DataFrame:
 
 
 # Drop column which are not use.
-def drop_invalid_columns(data: pd.DataFrame, columns_to_drop: List[str])-> pd.DataFrame:
+def drop_invalid_columns(data: pd.DataFrame, columns_to_drop: List[str]) -> pd.DataFrame:
     data = data.drop(columns_to_drop, axis=1)
     return data
 
 
 # Translate the column name from hebrew to english.
-def column_into_english(data:pd.DataFrame)->  pd.DataFrame:
+def column_into_english(data: pd.DataFrame) -> pd.DataFrame:
     data.columns = list(map(lambda x: columns_mapping.get(x, ' '), data.columns))
     return data
 
@@ -75,17 +76,31 @@ def remove_city_from_address(data: pd.DataFrame) -> pd.DataFrame:
     return data
 
 
+def remove_lines_with_nan(data: pd.DataFrame) -> pd.DataFrame:
+    # Filtering rows with NaN values in any column
+    return data.dropna()
+
+
+def insert_default_value(data: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    # Replacing NaN values in column 'B' with a default string 'No Value'
+    default_string = ' '
+    data[column_name] = data[column_name].fillna(default_string)
+    return data
+
+
 def main(source_path):
     output_path = Path(source_path).parent / 'output'
     output_path.mkdir(exist_ok=True)
-    columns_to_drop = ['Event Id', 'Id', 'חותמת', 'שם פרטי ומשפחה', 'שם וחתימת נותן ההפניה',
-                       'למען (לציין זהות הגוף או האדם שלמענו נעשית הפעולה ומקום הפעולה):', 'תאריך',
-                       'מלא תאריך סטטוס רישום']
     file = Path(source_path)
     data = pd.read_excel(file)
 
     data = drop_invalid_columns(data, columns_to_drop)
     data = column_into_english(data)
+    for column_name in columns_mapping.values():
+        if column_name == 'full name':
+            continue
+        data = insert_default_value(data, column_name)
+    data = remove_lines_with_nan(data)
     data = split_full_name(data)
     data = remove_city_from_address(data)
     replacement(data, output_path)
